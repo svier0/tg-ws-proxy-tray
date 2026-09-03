03112e47fc806f7c64f3ecd99b01876d410962cb
心衍 <vier@j7yx.com>
2026-09-03 22:54:32 +0800

    Update ipc.rs

--- src-tauri/src/ipc.rs
+++ src-tauri/src/ipc.rs
@@ -1,4 +1,7 @@
 
+use simple_tauri::simple_serve;
+use simple_tauri::config;
+
 /// 获取本体版本号
 #[tauri::command]
 pub fn ipc_version() -> String {
@@ -8,37 +11,37 @@ pub fn ipc_version() -> String {
 /// 获取服务端本号
 #[tauri::command]
 pub fn ipc_server_version() -> String {
-    "0.1.0-rc.6".to_string()
+    simple_serve::get_local_ver()
 }
 
 /// 获取最新版本号
 #[tauri::command]
 pub fn ipc_latest_ver() -> String {
-    simple_tauri::utils::get_gh_latest_ver("@deepseek-ai/dsh").to_string()
+    simple_tauri::utils::get_latest_ver("github","svier0/tg-ws-proxy-tary").to_string()
 }
 
 /// 获取服务端最新版本号
 #[tauri::command]
 pub fn ipc_server_latest_ver() -> String {
-    simple_tauri::utils::get_gh_latest_ver("@deepseek-ai/dsh").to_string()
+    simple_serve::get_latest_ver().ok().expect("REASON")
 }
 
-/// 获取配置列表
+/// 获取配置列表 TODO:
 #[tauri::command]
 pub fn ipc_config() {
 }
 
-/// 设置单个配置
+/// 设置单个配置 TODO:
 #[tauri::command]
 pub fn ipc_set_config() {
 }
 
-/// 批量设置配置
+/// 批量设置配置 TODO:
 #[tauri::command]
 pub fn ipc_set_configs() {
 }
 
-/// 更新本体
+/// 更新本体 TODO:
 #[tauri::command]
 pub fn ipc_update() {
 }
@@ -46,19 +49,21 @@ pub fn ipc_update() {
 /// 更新服务端
 #[tauri::command]
 pub fn ipc_server_update() {
+    simple_serve::check_update(config::get_bool("auto_update").expect("REASON")).ok();
 }
 
 /// 获取服务状态
 #[tauri::command]
-pub fn ipc_server_status() {
+pub fn ipc_server_status() -> String {
+    if simple_serve::is_running() { "true".to_string() } else { "false".to_string() }
 }
 
-/// 设置服务状态
+/// 设置服务状态 TODO:
 #[tauri::command]
 pub fn ipc_server_action() {
 }
 
-/// 读取服务日志
+/// 读取服务日志 TODO:
 #[tauri::command]
 pub fn ipc_server_logs() {
 }
